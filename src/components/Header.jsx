dc3bbd36dc7bdbfa3b9118485032114fe7b464f0
Fidane Tağızade <tagizadefidane@gmail.com>
2026-06-02 23:50:49 +0400

    feat: complete Header component structure and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
-function Header(){
-    return(
-        <div>
-            <header className="header-title">GeoTravel</header>
-            <div className="countries-container">
-                
-            </div>
-            
-        </div>
-    )
+function Header() {
+    return (
+        <header className="header-container">
+            <h1 className="header-title">GeoTravel</h1>
+            <nav className="nav-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/tools" className="nav-link">Tools</Link>
+                <Link to="/quiz" className="nav-link">Quiz</Link>
+            </nav>
+        </header>
+    );
 }
+
 export default Header;
