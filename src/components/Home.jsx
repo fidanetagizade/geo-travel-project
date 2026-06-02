6185eab8454d314d15800180d4c0723e31de2870
Fidane Tağızade <tagizadefidane@gmail.com>
2026-06-02 23:49:21 +0400

    feat: complete Home component structure and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
-import "./Home.css"
+import "./Home.css";
+import Filter from "./Filter";
 import Header from "./Header";
 import { useEffect, useState } from "react";
 
 function Home() {
-
     const [countries, setCountries] = useState([]);
+    const [visibleCount, setVisibleCount] = useState(15);
+    const [searchItem, setSearchItem] = useState("");
 
     useEffect(() => {
         fetch(`https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,cca3,borders`)
@@ -14,36 +16,53 @@ function Home() {
                 console.log(data);
             })
             .catch(err => console.log("Xəta baş verdi:", err));
+    }, []);
 
-    }, [])
+    const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchItem.toLowerCase())
+    );
 
     return (
         <div>
             <Header />
-            <div className="countries-container">
-                {countries.map((country) => {
-                    return (
-                        <div key={country.cca3} className="country-card">
+            <Filter setSearchItem={setSearchItem} />
+            <div className="main">
+                
+                {filteredCountries.length === 0 ? (
+                    <h2 className="not-found-msg">Country not found...</h2>
+                ) : (
+                    <div className="countries-container">
+                        {filteredCountries.slice(0, visibleCount).map((country) => {
+                            return (
+                                <div key={country.cca3} className="country-card">
+                                    <img
+                                        src={country.flags.png}
+                                        alt={`${country.name.common} bayrağı`}
+                                        className="country-flag"
+                                    />
+                                    <div className="country-details">
+                                        <h3>{country.name.common}</h3>
+                                        <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+                                        <p><strong>Region:</strong> {country.region}</p>
+                                        <p><strong>Capital:</strong> {country.capital ? country.capital[0] : "N/A"}</p>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
 
-                            <img
-                                src={country.flags.png}
-                                alt={`${country.name.common} bayrağı`}
-                                className="country-flag"
-                            />
-
-                            <div className="country-details">
-                                <h3>{country.name.common}</h3>
-                                <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
-                                <p><strong>Region:</strong> {country.region}</p>
-                                <p><strong>Capital:</strong> {country.capital ? country.capital[0] : "N/A"}</p>
-                            </div>
-                        </div>
-                    );
-                })}
+                
+                {filteredCountries.length > visibleCount && (
+                    <div className="btn-container">
+                        <button className="show-more-btn" onClick={() => setVisibleCount(visibleCount + 12)}>
+                            Show More
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
 }
-
 
 export default Home;
