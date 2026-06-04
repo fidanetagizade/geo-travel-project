ac6fa60f76777d9d7adc4566b8333086ebd50eed
Fidane Tağızade <tagizadefidane@gmail.com>
2026-06-04 15:47:26 +0400

    feat: add Favorite component

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,54 @@
+import "./Favorites.css"
+import Header from "./Header";
+
+function Favorites({ bucketList, setBucketList }) {
+
+    const removeFromFavorites = (country) => {
+        setBucketList(bucketList.filter(item => item.cca3 !== country.cca3));
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className="main">
+                <h1 className="favorite-input">
+                    My Favorite Countries!
+                </h1>
+                
+                {bucketList.length === 0 ? (
+                    <h2 className="not-found-msg" style={{ marginTop: "50px" }}>
+                        Your favorite list is empty...
+                    </h2>
+                ) : (
+                    <div className="countries-container">
+                        {bucketList.map((country) => (
+                            <div key={country.cca3} className="country-card">
+                                
+                                <button 
+                                    className="heart-btn" 
+                                    onClick={() => removeFromFavorites(country)}
+                                >
+                                    ❤️
+                                </button>
+
+                                <img
+                                    src={country.flags.png}
+                                    alt={`${country.name.common} bayrağı`}
+                                    className="country-flag"
+                                />
+                                <div className="country-details">
+                                    <h3>{country.name.common}</h3>
+                                    <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+                                    <p><strong>Region:</strong> {country.region}</p>
+                                    <p><strong>Capital:</strong> {country.capital ? country.capital[0] : "N/A"}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Favorites; 
